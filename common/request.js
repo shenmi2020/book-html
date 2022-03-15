baf96f4b72231f672ff79eb0eaf80a6a642f7ed0
shenmi2020 <shenmi2020@foxmail.com>
2022-03-15 21:20:30 +0800

    login

---
+++ common/request.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'http://127.0.0.1:8787'
+
+export  const myRequest = (options) => {
+  return new Promise((resolve, reject) => {
+    uni.request({
+      url: BASE_URL + options.url,
+      method: options.method || 'GET',
+      data: options.data || {},
+      timeout: 4000
+    }).then(res => {
+      if (res.data.code !== 0) {
+        return uni.showToast({
+          title: '接口异常'
+        })
+      }
+      resolve(res.data)
+    }).catch(err => {
+      uni.showToast({
+        title: '请求超时'
+      })
+      reject(err)
+    })
+  })
+}
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,13 +5,24 @@
       <text class="book">默认测试</text>
     </view>
     <view class="record">
-      <view class="month">3 月·支出</view>
+      <view class="month">3 月·支出1222333</view>
+      <view>400</view>
+      <view>收入 0 | 结余 -400</view>
     </view>
-    
+    <view class="list">
+      <view class="item">
+        <view>3月10日2022</view>
+        <view class="">
+          
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+import { myRequest } from '@/common/request.js'
+  
 export default {
   data() {
     return {
@@ -19,15 +30,31 @@ export default {
       title: "Hello",
     };
   },
+  
   onLoad() {
+    uni.login({
+      provider: 'weixin',
+      success: async function (loginRes) {
+        console.log('login', loginRes)
+        let info = await myRequest({
+          url: '/user/login',
+          method: 'POST',
+          data: {
+            code: loginRes.code
+          }
+        })
+        console.log('upload12:', info)
+      }
+    })
     // #ifdef MP-WEIXIN
     let menuButtonInfo = uni.getMenuButtonBoundingClientRect()
     this.topHeight = menuButtonInfo.top + menuButtonInfo.height + 10
     // #endif
-    
-    console.log('res:', menuButtonInfo)
   },
-  methods: {},
+  
+  methods: {
+    
+  },
 };
 </script>
 
@@ -45,6 +72,7 @@ export default {
     }
   }
   .record {
+    background-color: #cccccc;
     .month {
       font-size: 30rpx;
     }
