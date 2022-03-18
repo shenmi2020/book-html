b069fe4a25f267f2befb3972a7fb95dd83ae56f9
shenmi2020 <shenmi2020@foxmail.com>
2022-03-18 17:17:03 +0800

    index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,16 +4,16 @@
     <view class="status_bar" :style="{ height: topHeight + 'px' }">
       <text class="book">默认测试1</text>
     </view>
-    <view class="record">
+    <!-- <view class="record">
       <view class="month">3 月·支出1222333</view>
       <view>400</view>
       <view>收入 0 | 结余 -400</view>
-    </view>
+    </view> -->
     <view class="list">
-      <view class="item">
-        <view>3月10日2022</view>
-        <view class="">
-          
+      <view class="item" v-for="(val, key) in list" :key="key">
+        <view class="day">{{key}}</view>
+        <view class="apiece" v-for="item in val" :key="item.id">
+          <view class="price">{{item.remark + '--' + item.money}}</view>
         </view>
       </view>
     </view>
@@ -28,26 +28,50 @@ export default {
     return {
       topHeight: 56,
       title: "Hello",
+      list: {},
+      contro: false,
+      pageIndex: 1,
     }
   },
   
   onLoad() {
-    // 请求
-    this.first()
+    // 初始请求
+    this.fetchRecord()
     // #ifdef MP-WEIXIN
     let menuButtonInfo = uni.getMenuButtonBoundingClientRect()
     this.topHeight = menuButtonInfo.top + menuButtonInfo.height + 10
     // #endif
   },
   
+  onReachBottom() {
+    if (!this.contro) {
+      return false
+    }
+    this.fetchRecord(++this.pageIndex)
+  },
+  
   methods: {
     // 初始请求
-    async first() {
+    async fetchRecord(pageIndex = 1) {
+      this.contro = false
       let res = await myRequest({
-        url: '/user/demo2',
-        method: 'POST'
+        url: '/wx/record',
+        method: 'POST',
+        data: {
+          aid: 1,
+          pageIndex
+        }
       })
-      console.log('demo2:', res)
+      
+      res.data.map(item => {
+        if (!this.list.hasOwnProperty(item.day)) {
+          this.list[item.day] = []
+        }
+        this.list[item.day].push(item)
+      })
+      if (res.data.length == 10) {
+        this.contro = true
+      }
     },    
   },
 };
@@ -70,6 +94,25 @@ export default {
     background-color: #cccccc;
     .month {
       font-size: 30rpx;
+    }
+  }
+  .list {
+    .item {
+      margin: 50rpx 0;
+      .day {
+        height: 60rpx;
+        line-height: 60rpx;
+        background-color: #ffffcc;
+        font-size: 30rpx;
+      }
+      .apiece {
+        .price {
+          height: 80rpx;
+          line-height: 80rpx;
+          font-size: 40rpx;
+          margin: 20rpx 0;
+        }
+      }
     }
   }
 }
